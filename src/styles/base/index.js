2357f3f3ce84c4f60148bcce1a02105c814b6223
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-06-02 15:06:12 -0300

    cria os componentes basicos de color, reset, spacing e base

---
+++ src/styles/base/index.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const Base = createGlobalStyle`
+  body {
+    font-family: 'Roboto Slab', serif;
+    background-color: var(--color-first);
+  }
+`;
+
+export default Base;
---
+++ src/styles/settings/colors.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const Colors = createGlobalStyle`
+  :root {
+    --color-base: #3A4042;
+    --color-zero: #FFFFFF;
+    --color-first: #F25A70;
+    --color-second: #191637;
+    --color-third: #EAE6DA;
+  }
+`;
+
+export default Colors;
